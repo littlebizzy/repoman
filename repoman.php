31b434a6ec8136cb66fd29cfea5ff8f2f3ef3e4b
Jesse Nickles <jesse@littlebizzy.com>
2024-12-06 14:57:48 +0700

    Update repoman.php

--- repoman.php
+++ repoman.php
@@ -3,7 +3,7 @@
 Plugin Name: RepoMan
 Plugin URI: https://www.littlebizzy.com/plugins/repoman
 Description: Install public repos to WordPress
-Version: 1.7.5
+Version: 1.7.6
 Requires PHP: 7.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
